7f82e43198b4ded1ec6e6326017ac0a9301ef3c3
Baher Talal <57946827+baher-talal@users.noreply.github.com>
2021-04-01 11:04:14 +0200

    order page

--- resources/views/front/order.blade.php
+++ resources/views/front/order.blade.php
@@ -1,42 +1,47 @@
 @extends("front.master")
 
 @section("content")
-<div class="container">
-  <h2>List Order</h2>
-  <p>List All Previous Order With Status:</p>
-  @if(session()->has("success"))
-  <p class="text-success"> {{ session()->get("success") }} </p>
-  @endif
-  @if(session()->has("faild"))
-  <p class="text-danger"> {{ session()->get("faild") }} </p>
-  @endif
-  <div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>orderNumber</th>
-        <th>orderFinalTotal</th>
-        <th>currencySymbol</th>
-        <th>orderCreateDate</th>
-        <th>orderCurrentStatus</th>
-        <th>orderPaymentMethod</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-    @foreach($orders as $order)
-      <tr>
-        <td>{{  $order->orderNumber }}</td>
-        <td>{{  $order->orderFinalTotal }}</td>
-        <td>{{  $order->currencySymbol }}</td>
-        <td>{{  $order->orderCreateDate }}</td>
-        <td>{{  $order->orderCurrentStatus }}</td>
-        <td>{{  $order->orderPaymentMethod }}</td>
-        <td> <a class="btn btn-gray text-success" href="{{ route('front.order.details', ['order_id' => $order->orderNumber]) }}"> Show </a> </td>
-      </tr>
-    @endforeach
-    </tbody>
-  </table>
+<section class="order_sec">
+  <div class="row m-0">
+    <div class="col-12">
+      <h2 class="text-center">List Order</h2>
+      <p class="text-center">List All Previous Order With Status:</p>
+      @if(session()->has("success"))
+      <p class="text-success"> {{ session()->get("success") }} </p>
+      @endif
+      @if(session()->has("faild"))
+      <p class="text-danger"> {{ session()->get("faild") }} </p>
+      @endif
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>orderNumber</th>
+              <th>orderFinalTotal</th>
+              <th>currencySymbol</th>
+              <th>orderCreateDate</th>
+              <th>orderCurrentStatus</th>
+              <th>orderPaymentMethod</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            @foreach($orders as $order)
+            <tr class="table_grid">
+              <td><span class="title">Order Number:</span> <b class="title_value">{{ $order->orderNumber }}</b></td>
+              <td><span class="title">Order Final Total:</span> <b class="title_value">{{ $order->orderFinalTotal }}</b></td>
+              <td><span class="title">Currency Symbol:</span> <b class="title_value">{{ $order->currencySymbol }}</b></td>
+              <td><span class="title">Order Create Date:</span> <b class="title_value">{{ $order->orderCreateDate }}</b></td>
+              <td><span class="title">Order Current Status:</span> <b class="title_value">{{ $order->orderCurrentStatus }}</b></td>
+              <td><span class="title">Order Payment Method:</span> <b class="title_value">{{ $order->orderPaymentMethod }}</b></td>
+              <a class="btn_show btn" href="{{ route('front.order.details', ['order_id' => $order->orderNumber]) }}"> Show </a>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
-</div>
+</section>
 @stop
