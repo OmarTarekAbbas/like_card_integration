2ac4775dc1cd253ca2c4f208638c233693f95eea
mohamed mahmoud <m.mahmoud@iVAS.local>
2021-04-18 13:13:56 +0200

    handle client profile

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -119,7 +119,7 @@ class OrderController extends Controller
       $this->payment->buyItems($request->all());
       if($this->payment->isSuccess()){
         $response = $this->payment->getData();
-        session()->flash("Your Order Create Successfully");
+        session()->flash("success", "Your Order Create Successfully");
         return redirect()->route("front.order.details",["order_id" => $response]);
       }
       session()->flash("faild", $this->payment->getError());
