3fa9e8545ff23a16853ad621c1ff35dd2dab7564
mohamed mahmoud <m.mahmoud@iVAS.local>
2021-04-20 11:37:41 +0200

    send mail to admin with current balance in like card

--- routes/front.php
+++ routes/front.php
@@ -12,7 +12,7 @@ define("ShortCode", "50230");
 
 define('enable_dcb', false);
 define('balance_limit', 40);
-define('admin_mail', "m.mahmoud@ivas.com.eg");
+define('admin_mail', "mohammed_hs552yahoo.com");
 
 Route::get("/home","HomeController@index")->name("front.home");
 Route::get("/category/{parent_id}","HomeController@listCategoryChilds")->name("front.category");
