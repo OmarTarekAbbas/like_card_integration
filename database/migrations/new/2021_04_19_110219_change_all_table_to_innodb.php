161347823bffb3c0736f64910da6318664d84002
mohamed mahmoud <m.mahmoud@iVAS.local>
2021-05-20 10:46:27 +0200

    make migrations for chnage myisam to innodb

---
+++ database/migrations/new/2021_04_19_110219_change_all_table_to_innodb.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeAllTableToInnodb extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      $sql = "SELECT CONCAT('ALTER TABLE ',TABLE_NAME,' ENGINE=InnoDB;') as table_name
+      FROM INFORMATION_SCHEMA.TABLES
+      WHERE ENGINE='MyISAM'
+      AND table_schema = 'like_card';";
+
+      $all_table = DB::select(DB::raw($sql));
+
+      foreach ($all_table as  $tableSql) {
+        DB::statement($tableSql->table_name);
+      }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
