a124409fe04883b4b2acce31e9d8466095901543
mohamed mahmoud <m.mahmoud@iVAS.local>
2021-05-30 16:32:21 +0200

    handle call back in myfatoorah

--- app/Myfatoorah.php
+++ app/Myfatoorah.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Myfatoorah extends Model
 {
-    protected $fillable = ['url', 'request', 'response', 'type', 'order_id', 'invoice_id', 'payment_url', 'payment_method', 'invoice_status', 'Transaction_status'];
+    protected $fillable = ['url', 'request', 'response', 'type', 'order_id', 'invoice_id', 'payment_url', 'payment_method', 'invoice_status', 'transaction_status'];
 }
